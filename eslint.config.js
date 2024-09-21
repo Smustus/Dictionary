5b034b053622f67ab988c0914f17275ffcfb95f6
Smustus <rikardfors_91@hotmail.com>
2024-09-22 00:32:12 +0200

    added code
    adjusting to flat config, adding stylelint

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,48 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import globals from 'globals';
+import js from '@eslint/js';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import eslintConfigPrettier from "eslint-config-prettier";
 
+//https://eslint.org/docs/latest/use/configure/configuration-files
+//https://github.com/prettier/eslint-config-prettier#installation
+
+const prettierRules = {
+  'arrow-body-style': 'off',
+  'prefer-arrow-callback': 'off',
+};
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
-    "extends": ["react-app"],
-    "rules": {
-      "react/react-in-jsx-scope": "off"
-    }
-  }
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'prefer-const': 'error',
+      'no-console': 'warn',
+      ...prettierRules,
+    },
+  },
+  eslintConfigPrettier
 ];
-
